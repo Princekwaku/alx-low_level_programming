95a76da1a69ceb25ffd14e751f94b9c4591be3ab
Princekwaku <princekwakuantwi@gmail.com>
2023-03-20 17:27:23 -0700

    variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - prints numbers from 0 to 9
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+
+for (i = 97; i < 123; i++)
+{
+if (i != 101 && i != 113)
+{
+putchar(i)'
+}
+}
+putchar('\n');
+return (0);
+}
